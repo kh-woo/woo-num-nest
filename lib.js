5c4bd6269ad185a1f50b848a8e80d7663d1decff
khw <khw040309@naver.com>
2024-11-04 02:14:28 +0900

    assigment 1_1

--- lib.js
+++ lib.js
@@ -2,12 +2,24 @@ function avg(numbers) {
   let s = numbers.reduce((prev, curr) => prev + curr, 0);
   return s / numbers.length;
   }
-  function prime(num) {
-  return false;
+
+function prime(n) {
+  if (n <= 1) return false;
+  for (let i = 2; i <= Math.sqrt(n); i++) {
+      if (n % i === 0) return false;
   }
-  function factorial(num) {
-  return -1;
+  return true;
+}
+
+function factorial(n) {
+  if (n < 0) return undefined; 
+  let result = 1;
+  for (let i = 2; i <= n; i++) {
+      result *= i;
   }
+  return result;
+}
+
   module.exports = {
   avg,
   prime,
--- lib.test.js
+++ lib.test.js
@@ -1,8 +1,26 @@
 const { test, expect } = require("@jest/globals");
 const lib = require("./lib");
+
 test("avg([3, 5, 7]) should be 5", () => {
-expect(lib.avg([3, 5, 7])).toBe(5);
+    expect(lib.avg([3, 5, 7])).toBe(5);
 });
+
 test("avg([-5, 5]) should be 0", () => {
-expect(lib.avg([-5, 5])).toBe(0);
+    expect(lib.avg([-5, 5])).toBe(0);
+});
+
+test("prime(5) should return true", () => {
+    expect(lib.prime(5)).toBe(true);
+});
+
+test("prime(4) should return false", () => {
+    expect(lib.prime(4)).toBe(false);
+});
+
+test("factorial(3) should return 6", () => {
+    expect(lib.factorial(3)).toBe(6);
+});
+
+test("factorial(5) should return 120", () => {
+    expect(lib.factorial(5)).toBe(120);
 });
